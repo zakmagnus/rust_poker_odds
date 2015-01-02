29da96ce0d578235b8115b5c8474b41b98432e6a
ZAK Magnus <zakmagnus@gmail.com>
2015-01-02 15:43:33 -0800

    Initial commit: cards crate containing Card struct

---
+++ cards/src/lib.rs
@@ -0,0 +1,24 @@
+enum Suit {
+    Spades,
+    Hearts,
+    Clubs,
+    Diamonds,
+}
+
+enum Rank {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+}
+
+struct Card { suit: Suit, rank: Rank }
