a61c6c1b16cf627e7fc8d32c76dabff94c2bc420
ZAK Magnus <zakmagnus@gmail.com>
2015-01-02 21:10:32 -0800

    Order the ranks
    By implementing PartialEq and PartialOrd traits,
    which are themselves just comparing bare numbers
    that are coming out of mapping function.
    
    I don't think there's a way to statically
    associate values with enum variants like in Java,
    nor do the variants automatically have ascending
    numerical values, like in C. So, I had to write
    a function that does the mapping.
    
    To get total ordering, I had to add a deriving
    annotation, since the relevant trait (Eq)
    doesn't seem to have any functions for me to
    impl. Not sure if this will work correctly.

--- cards/src/lib.rs
+++ cards/src/lib.rs
@@ -1,3 +1,8 @@
+use std::option::Option;
+use std::cmp::Ord;
+use std::cmp::PartialOrd;
+use std::cmp::Ordering;
+
 pub enum Suit {
     Spades,
     Hearts,
@@ -5,20 +10,60 @@ pub enum Suit {
     Diamonds,
 }
 
+#[deriving(Eq, Ord)]
 pub enum Rank {
     Ace,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
     King,
+    Queen,
+    Jack,
+    Ten,
+    Nine,
+    Eight,
+    Seven,
+    Six,
+    Five,
+    Four,
+    Three,
+    Two,
 }
 
+// This uses a high ace.
+fn rank_to_index(rank: & Rank) -> u8 {
+    match *rank {
+        Rank::Ace => 14,
+        Rank::King => 13,
+        Rank::Queen => 12,
+        Rank::Jack => 11,
+        Rank::Ten => 10,
+        Rank::Nine => 9,
+        Rank::Eight => 8,
+        Rank::Seven => 7,
+        Rank::Six => 6,
+        Rank::Five => 5,
+        Rank::Four => 4,
+        Rank::Three => 3,
+        Rank::Two => 2,
+    }
+}
+
+// Ace is high in this ordering.
+impl PartialOrd for Rank {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let this_index = rank_to_index(self);
+        let other_index = rank_to_index(other);
+        let comparison = this_index.cmp(&other_index);
+        Option::Some(comparison)
+    }
+}
+
+impl PartialEq for Rank {
+    fn eq(&self, other: &Rank) -> bool {
+        let this_index = rank_to_index(self);
+        let other_index = rank_to_index(other);
+        this_index.eq(&other_index)
+    }
+}
+
+
 pub struct Card { suit: Suit, rank: Rank }
+
