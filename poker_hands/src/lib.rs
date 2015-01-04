16c0956af5b0d3caabb780058d85faada42b5845
ZAK Magnus <zakmagnus@gmail.com>
2015-01-04 03:45:03 -0800

    Use arrays instead of tuples for kickers
    Tuples probably weren't what I really wanted.
    
    Arrays of varying sizes can be passed to a
    single function, which is nice. Maybe I'll
    need this ability later.

--- poker_hands/src/lib.rs
+++ poker_hands/src/lib.rs
@@ -2,16 +2,15 @@ extern crate cards;
 
 use cards::Rank;
 
-// Rank tuples are used for kickers. They should be sorted descending.
+// Rank arrays are used for kickers. They should be sorted descending.
 
-struct FiveOrderedRanks(Rank, Rank, Rank, Rank, Rank);
-enum Hands {
-    HiCard        {ranks: FiveOrderedRanks},
-    Pair          {rank: Rank, kickers: (Rank, Rank, Rank)},
+enum Hand {
+    HiCard        {ranks: [Rank, ..5]},
+    Pair          {rank: Rank, kickers: [Rank, ..5]},
     TwoPair       {hiRank: Rank, loRank: Rank, kicker: Rank},
-    Trips         {rank: Rank, kickers: (Rank, Rank)},
+    Trips         {rank: Rank, kickers: [Rank, ..2]},
     Straight      {hiRank: Rank},
-    Flush         {ranks: FiveOrderedRanks},
+    Flush         {ranks: [Rank, ..5]},
     FullHouse     {threeOf: Rank, twoOf: Rank},
     Quads         {rank: Rank, kicker: Rank},
     StraightFlush {hiRank: Rank},
