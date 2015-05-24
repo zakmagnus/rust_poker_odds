25803428f32ca0165671c747fb38ae64f0d36e2e
ZAK Magnus <zakmagnus@gmail.com>
2015-05-24 00:34:19 -0700

    Start poker_odds crate
    Finally!
    
    This one is actually a CLI, not a library. Its entire
    functionality is already described in comments. Devil's in
    the details.

---
+++ poker_odds/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+    //TODO read cli args
+
+    //TODO get some hole cards
+    //TODO numSims times:
+    //TODO    pick a random community
+    //TODO    make hands for all players
+    //TODO    figure out winners or chops
+    //TODO    note down what happened
+    //TODO print stats of what happened
+}
